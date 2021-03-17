df3dd5c2c51cbeb6dfc0de7eb43d8300471a2977
kitmehta77 <kit.mehta@gmail.com>
2021-03-17 15:30:18 +1300

    updating

--- HTCS5604/guessANumber/library.py
+++ HTCS5604/guessANumber/library.py
@@ -3,3 +3,10 @@ from random import randint
 def generateANumber():
     return randint(1,99)
 
+def isOutOfTry(tryTimes, TotalTimes):
+    if tryTimes <= TotalTimes:
+        return False
+    return True
+
+def tryTimesLeft(tryTimes, TotalTimes):
+    return TotalTimes - tryTimes
