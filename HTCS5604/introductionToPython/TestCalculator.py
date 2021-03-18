0bf3804d6f57565dbd1c3e29ee6ea7b1c4c325cf
kitmehta77 <kit.mehta@gmail.com>
2021-03-18 16:32:20 +1300

    unittest 1

--- HTCS5604/introductionToPython/TestCalculator.py
+++ HTCS5604/introductionToPython/TestCalculator.py
@@ -6,5 +6,21 @@ class TestCalculator(unittest.TestCase):
         result = g.add(5,6)
         self.assertEqual(11,result)
 
+    def test_subtract(self):
+        result = g.subtract(8,2)
+        self.assertEqual(6,result)
+
+    def test_multiply(self):
+        result = g.multiply(10,3)
+        self.assertEqual(30,result)
+
+    def test_division(self):
+        result = g.divide(10,2)
+        self.assertEqual(5,result)
+
+    def test_power(self):
+        result = g.power(2,2)
+        self.assertEqual(4,result)
+
 if __name__ == '__main__':
     unittest.main()
